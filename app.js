c2d91b7404e9e426cc587df5745d02ae2546d9c2
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-04-08 06:56:18 +0300

    Static Files and Template Engine

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(blog)
-});
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/index', (req, res) => {
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+app.get('/post', (req, res) => {
+    res.render('post')
+})
 
 const port = 3000;
 
